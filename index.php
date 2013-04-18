180c84ab5a2862bbd52dbf72714b372def0dd184
web-com <luka.bracanovic@web-com.hr>
2013-04-18 14:21:42 +0200

    Commit

--- index.php
+++ index.php
@@ -1,2 +1,317 @@
-<?php
-die('git succesfull i kod mene - vanja');
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+<title></title>
+<style type="text/css">
+@font-face{
+	font-family:custom;
+	src:url(css/CONSTAN_0.TTF);
+}
+body{
+	background: url(images/pozadina.jpg);
+	margin:0;
+	padding:0;
+	font-family:custom;
+}
+h2{
+	font-family:custom;
+	text-transform:uppercase;
+	color:#646464;
+	margin:0 0 40px 0;
+	padding:0 0 0 0;
+	height:28px;
+	background:url(images/shadow_uio.png) no-repeat -10px 19px;
+}
+h2:nth-child(3){
+	text-align:right;
+	background:url(images/shadow_zasto.png) no-repeat 580px 20px;
+	margin-top:55px;
+}
+h3{
+	color:#636363;
+	font-size:16px;
+}
+p{
+	font-size:15px;
+	line-height:22px;
+	color:#818080;
+}
+.holder{
+	max-width:960px;
+	margin:0 auto;
+	background:#fff url(images/gradient.png) repeat-x top center;
+	box-shadow:0 0 30px rgba(000,000,000,0.5);
+}
+.header{
+	margin:0 0 100px 0;
+	background:#fff url(images/line_sep.png) no-repeat bottom center;
+	padding:0;
+	height:115px;
+	position:fixed;
+}
+.header ul{
+	list-style:none;
+	margin:0;
+	padding:0;
+	border:0;
+	outline:0;
+}
+.header ul li{
+	float:left;
+	padding:60px 0 20px 20px;
+	text-align:center;
+	width:149px;
+	border-right:3px solid #fff;
+	text-transform:uppercase;
+	color:#fff;
+	font-size:22px;
+	text-shadow:2px 2px 2px #000;
+	cursor:pointer;
+}
+.header ul li:first-child{
+	background:#fff url(images/logo.png) no-repeat center center;
+	width:248px;
+	min-height:110px;
+	padding:0 0 0 20px;
+}
+.header ul li:nth-child(2){
+	background:url(images/menu/usluge.png) no-repeat top center;
+}
+.header ul li:nth-child(3){
+	background:url(images/menu/klijenti.png) no-repeat top center;
+}
+.header ul li:nth-child(4){
+	background:url(images/menu/kontakt.png) no-repeat top center;
+}
+.header ul li:nth-child(5){
+	background:url(images/menu/linkovi.png) no-repeat top center;
+	width:153px;
+	border-right:0px;
+}
+.page{
+	padding:200px 48px 270px 36px;
+	min-height:600px;
+}
+#page1{
+	background:url(images/line_sep.png) no-repeat center 780px,url(images/knjiga.png) no-repeat center 660px;
+}
+#page1 p:nth-child(4){
+	text-align:right;
+}
+
+#page2{
+	background:url(images/line_sep.png) no-repeat center 1000px;
+}
+.usluga{
+	float:left;
+	min-width:400px;
+	max-width:400px;
+	margin:10px 0;
+	min-height:200px;
+}
+.usluga:nth-child(even){
+	margin-right:40px;
+}
+.usluga ul{
+	color:#7a7a7a;
+	list-style:dot;
+	font-size:14px;	
+	line-height:22px;
+}
+#page3{
+	background:url(images/line_sep.png) no-repeat center 900px;
+}
+.klijenti{
+	margin-top:120px;
+}
+.klijenti p{
+	float:left;
+	max-width:420px;	
+	min-width:420px;
+	text-align:left;
+	font-size:18px;
+	line-height:20px;
+	margin:20px 0 0 0;
+	color:#6d6c6c;
+}
+.klijneti p:nth-child(even){
+	margin-right:20px;
+}
+
+#page4{
+	background:url(images/line_sep.png) no-repeat center 850px;
+}
+#page4 div{
+	float:left;
+	min-width:400px;
+}
+#page4 div:nth-child(even){
+	margin:0 0 0 25px;
+	padding-top:10px;
+}
+#page4 div .karta{
+	float:none;
+	border:1px solid #c6c6c6;
+	padding:5px 5px 5px 5px;
+	min-width:396px;
+	margin:0;
+}
+#page4 span{
+	color:#434343;
+	font-size:16px;
+	font-weight:bold;
+	margin-right:10px;
+}
+.lokacija{
+	margin-top:0px;
+	margin-bottom:5px;
+	font-size:18px;
+}
+#page4 div .logo{
+	padding:0;
+	margin:20px 0 0 0;
+	background:url(images/logo_kontakt.png) no-repeat left center;
+	height:75px;
+}
+</style>
+</head>
+<body>
+<div class="holder">
+	<div class="header">
+    <ul>
+        <li></li>
+	    <li>Usluge</li>
+    	<li>Klijenti</li>
+        <li>Kontakt</li>
+        <li>Linkovi</li>
+    </ul>
+    </div>
+    <div id="page1" class="page">
+        <h2>O nama</h2>
+        <p>
+            Nudimo kompletnu uslugu knjigovodstva i računovodstva za poduzeća, obrte (standardne obrtnike i umjetnike) i udruge.<br>
+            Redovitim praćenjem zakonskih i poreznih propisa te hrvatskih i međunarodnih računovodstvenih standarda u <br>
+            mogućnosti smo pružiti stručan i profesionalan pristup.
+        </p>
+        <h2>Zašto odabrati nas</h2>
+        <p>
+            Dvadeset godina iskustva te mnoštvo zadovoljnih klijenata naša su glavna referenca.<br>
+            Cilj nam je pružiti klijentima kvalitetnu, brzu i efikasnu uslugu koja će dodati stvarnu vrijednost vašem poslovanju.
+        </p>
+    </div>
+    <div id="page2" class="page">
+        <h2>Usluge</h2>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
+            <p>
+				<ul>
+                    <li>dnevnik i glavna knjiga financijskog knjigovodstva</li>
+                    <li>knjige ulaznih (U-RA) i izlaznih (I-RA) računa</li>
+                    <li>knjiga osnovnih sredstava (evidencije nematerijalne i materijalne imovine)</li>
+                    <li>knjiga blagajne</li>
+                </ul>
+            </p>
+        </div>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
+            <p>
+				<ul>
+                    <li>mjesečni/kvartalni obračun poreza na dodanu vrijednost (PDV)</li>
+                    <li>godišnji obračun poreza na dodanu vrijednost (PDV-K)</li>
+                    <li>obračun kamata, tečajnih razlika i amortizacije osnovnih sredstava</li>
+                    <li>obračun putnih naloga</li>
+                </ul>
+            </p>
+        </div>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
+            <p>
+				<ul>
+                    <li>obračun plaće , ugovora o djelu i autorskih naknada</li>
+                    <li>priprema obrazaca uz plaću <br>
+                       - R-Sm za FINU (REGOS)<br>
+                       - ID, IPP za Poreznu Upravu
+                    </li>
+                    <li>priprema ostalih obrazaca vezanih uz dohodak: IDD, IP, IDD-1</li>
+                </ul>
+            </p>
+        </div>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
+            <p>
+				<ul>
+                    <li>račun dobiti i gubitka (RDG), bilanca (BIL) i bilješke uz financijske izvještaje</li>
+                    <li>tromjesečni statistički izvještaj (TSI-POD)</li>
+                    <li>godišnji financijski izvještaj (GFI-POD) za statistiku i javnu objavu</li>
+                    <li>prijava poreza na dobit (PD)</li>
+                </ul>
+            </p>
+        </div>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
+            <p>
+				<ul>
+                    <li>praćenje stanja obveza za javna davanja prema Poreznoj Upravi</li>
+                    <li>saldakonti kupaca i dobavljača</li>
+                    <li>izrada naloga za plaćanje (virmana)</li>
+                    <li>vođenje nadzornih knjiga za poslovanje s inozemstvom</li>
+                    <li>izvještavanje HNB-u o poslovima s inozemstvom</li>
+                    <li>priprema ostalih izvještaja prema željama i potrebama klijenta</li>
+                </ul>
+            </p>
+        </div>
+    </div>
+    <div id="page3" class="page">
+    	<h2>Klijenti</h2>
+        <p>
+            Nudimo kompletnu uslugu knjigovodstva i računovodstva za poduzeća, obrte (standardne obrtnike i umjetnike) i udruge.<br>
+            Redovitim praćenjem zakonskih i poreznih propisa te hrvatskih i međunarodnih računovodstvenih standarda u <br>
+            mogućnosti smo pružiti stručan i profesionalan pristup.
+        </p>
+        <div class="klijenti">
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        </div>
+    </div>
+    <div id="page4" class="page">
+    	<h2>Kontakt</h2>
+        <div>
+        	<p><span>tel:</span>+385 52 2389395 </p>
+        	<p><span>mob:</span>+385 98 334466</p>
+        	<p><span>email:</span>icapara@gmail.com</p>
+        	<p><span>oib:</span>02410562314</p>
+        	<p><span>mb:</span>01026054 </p>
+            <p><span>adresa:</span> Posavski Ljudevita 2, Pula</p>
+            <p>Bajta d.o.o.</p>
+            <div class="logo"></div>
+        </div>
+        <div>
+			<p class="lokacija">Pula, Croatia</p>
+        	<div class="karta">	
+                <iframe width="396" height="400" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=+Posavski+Ljudevita+2,+Pula&amp;aq=&amp;sll=44.881418,13.860626&amp;sspn=0.10497,0.209255&amp;ie=UTF8&amp;hq=&amp;hnear=Ulica+Ljudevita+Posavskog+2,+52100,+Pula,+Istarska+%C5%BEupanija,+Croatia&amp;t=m&amp;ll=44.860736,13.832817&amp;spn=0.097342,0.135612&amp;z=12&amp;iwloc=A&amp;output=embed"></iframe>
+            </div>
+        </div>
+    </div>
+    <div id="page5" class="page"></div>
+</div>
+<script>
+	$(".header") >$(" ul li").click(function(e) {
+		x=$(this).index() + 1;
+		$('body').animate({
+		 scrollTop: $("#page"+x).offset().top
+		 }, 800);
+	 });
+</script>
+</body>
+</html>
