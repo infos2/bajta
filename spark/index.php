6c343b238c9a733620c43664fafb7923fa5e8523
Vanja Retkovac <vanja.retkovac@gmail.com>
2013-04-27 09:26:34 +0200

    organizacija fajlova za admin

--- spark/index.php
+++ spark/index.php
@@ -9,8 +9,20 @@ if (isset($get->logout)) userLogout();
 if (!userLoggedIn()) require 'login.php';
 $user = getUser();
 
+/* HTML */
 if(!ob_start("ob_gzhandler")) ob_start('compress');
 $PART = new TemplateParts();
+
+/* TAB-MODULE set LOGIC */
+$module = !empty($get->m) ? $get->m : false;
+$action = !empty($get->a) ? $get->a : false;
+$req_tab = $tab = !empty($get->t) ? $get->t : 'pregled'; #default
+$req_mod = !empty($module) ? '_'.$module : false;
+$req_fpt = $req_tab.$req_mod.'.php'; #TODO
+
+/* TAB-MODULE-ACTION display LOGIC */
+if ($action or !empty($post)) file_exists($sitepath.'admin/logic/'.$req_fpt) ? require 'logic/'.$req_fpt : die('Pogreska logike');
+file_exists($sitepath.'admin/tabs/'.$req_fpt) ? require 'tabs/'.$req_fpt : die('Pogreska sustava');
 
 $PART->live_site = $live_site;
 $PART->user_name = $user->ime;
