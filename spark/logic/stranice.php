486e06cbdb38738f8fd87da2d88f91b0d239eb04
web-com <luka.bracanovic@web-com.hr>
2013-04-29 17:15:11 +0200

    logic stranice

---
+++ spark/logic/stranice.php
@@ -0,0 +1,20 @@
+<?php
+execute();
+
+function execute(){
+	global $get,$post;
+	$langs=db_getLanguages();
+	foreach($langs as $lang){
+		$prijevod = new object();
+		$prijevod->id_stranice = intval($get->id);
+		$prijevod->ln = $lang->ln;
+		$naslov_attrName = 'naslov_'.$prijevod->ln;
+		$prijevod->naslov = $post->$naslov_attrName;
+		$prijevod->url = prepareURI($prijevod->naslov);
+		if (empty($prijevod->naslov)) continue; // možda izbrisati prijevod ?
+		$sadrzaj_attrName = 'sadrzaj_'.$prijevod->ln;
+		$prijevod->sadrzaj = $post->$sadrzaj_attrName;
+		postojiPrijevod($prijevod->id_stranice,$prijevod->ln) ? db_updatePrijevod($prijevod) : db_insertPrijevod($prijevod);
+	}
+}
+
