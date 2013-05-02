cadaa7f46be5f8bd9939dd7e5b16008e86205f3b
web-com <luka.bracanovic@web-com.hr>
2013-05-02 22:55:02 +0200

    dobro sad nisam siguran jeli dobro napravljeno, ali uglavnom prebacio sam index1 i ovo ostalo s 1 na bez 1..

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
@@ -6,167 +5,149 @@
 <title></title>
 <link rel="stylesheet" type="text/css" href="css/front.css">
 <link rel="stylesheet" type="text/css" href="css/responsive.css">
+
 </head>
 <body>
-	<div id="holder">
-		<div id="header">
+<div id="holder">
+	<div id="header">
+    <ul>
+        <li>Bajta</li>
+	    <li>Usluge</li>
+    	<li>Klijenti</li>
+        <li>Kontakt</li>
+        <li>Linkovi</li>
+    </ul>
+    </div>
+    <div id="page1" class="page">
+        <h2>O nama</h2>
+        <p>
+            Nudimo kompletnu uslugu knjigovodstva i računovodstva za poduzeća, obrte (standardne obrtnike i umjetnike) i udruge.
+            Redovitim praćenjem zakonskih i poreznih propisa te hrvatskih i međunarodnih računovodstvenih standarda u 
+            mogućnosti smo pružiti stručan i profesionalan pristup.
+        </p>
+        <h2>Zašto odabrati nas</h2>
+        <p>
+            Dvadeset godina iskustva te mnoštvo zadovoljnih klijenata naša su glavna referenca.
+            Cilj nam je pružiti klijentima kvalitetnu, brzu i efikasnu uslugu koja će dodati stvarnu vrijednost vašem poslovanju.
+        </p>
+    </div>
+    <div id="page2" class="page">
+        <h2>Usluge</h2>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
 			<ul>
-				<li>Bajta O nama</li>
-				<li>Usluge</li>
-				<li>Klijenti</li>
-				<li>Kontakt</li>
-				<li>Linkovi</li>
+	            <li>dnevnik i glavna knjiga financijskog knjigovodstva</li>
+	            <li>knjige ulaznih (U-RA) i izlaznih (I-RA) računa</li>
+	            <li>knjiga osnovnih sredstava (evidencije nematerijalne i materijalne imovine)</li>
+	            <li>knjiga blagajne</li>
 			</ul>
-		</div>
-		<div id="page1" class="page">
-			<h2>O nama</h2>
-			<p>Nudimo kompletnu uslugu knjigovodstva i računovodstva za poduzeća,
-				obrte (standardne obrtnike i umjetnike) i udruge. Redovitim
-				praćenjem zakonskih i poreznih propisa te hrvatskih i međunarodnih
-				računovodstvenih standarda u mogućnosti smo pružiti stručan i
-				profesionalan pristup.</p>
-			<h2>Zašto odabrati nas</h2>
-			<p>Dvadeset godina iskustva te mnoštvo zadovoljnih klijenata naša su
-				glavna referenca. Cilj nam je pružiti klijentima kvalitetnu, brzu i
-				efikasnu uslugu koja će dodati stvarnu vrijednost vašem poslovanju.
-			</p>
-		</div>
-		<div id="page2" class="page">
-			<h2>Usluge</h2>
-			<div class="usluga">
-				<h3>Vođenje poslovnih knjiga</h3>
+        </div>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
+			<ul>
+                <li>mjesečni/kvartalni obračun poreza na dodanu vrijednost (PDV)</li>
+                <li>godišnji obračun poreza na dodanu vrijednost (PDV-K)</li>
+                <li>obračun kamata, tečajnih razlika i amortizacije osnovnih sredstava</li>
+            	<li>obračun putnih naloga</li>
+            </ul>
+        </div>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
 				<ul>
-					<li>dnevnik i glavna knjiga financijskog knjigovodstva</li>
-					<li>knjige ulaznih (U-RA) i izlaznih (I-RA) računa</li>
-					<li>knjiga osnovnih sredstava (evidencije nematerijalne i
-						materijalne imovine)</li>
-					<li>knjiga blagajne</li>
-				</ul>
-			</div>
-			<div class="usluga">
-				<h3>Vođenje poslovnih knjiga</h3>
+                    <li>obračun plaće , ugovora o djelu i autorskih naknada</li>
+                    <li>priprema obrazaca uz plaću <br/>
+                       - R-Sm za FINU (REGOS)<br/>
+                       - ID, IPP za Poreznu Upravu
+                    </li>
+                    <li>priprema ostalih obrazaca vezanih uz dohodak: IDD, IP, IDD-1</li>
+                </ul>
+        </div>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
 				<ul>
-					<li>mjesečni/kvartalni obračun poreza na dodanu vrijednost (PDV)</li>
-					<li>godišnji obračun poreza na dodanu vrijednost (PDV-K)</li>
-					<li>obračun kamata, tečajnih razlika i amortizacije osnovnih
-						sredstava</li>
-					<li>obračun putnih naloga</li>
-				</ul>
-			</div>
-			<div class="usluga">
-				<h3>Vođenje poslovnih knjiga</h3>
+                    <li>račun dobiti i gubitka (RDG), bilanca (BIL) i bilješke uz financijske izvještaje</li>
+                    <li>tromjesečni statistički izvještaj (TSI-POD)</li>
+                    <li>godišnji financijski izvještaj (GFI-POD) za statistiku i javnu objavu</li>
+                    <li>prijava poreza na dobit (PD)</li>
+                </ul>
+        </div>
+        <div class="usluga">
+            <h3>Vođenje poslovnih knjiga</h3>
 				<ul>
-					<li>obračun plaće , ugovora o djelu i autorskih naknada</li>
-					<li>priprema obrazaca uz plaću <br/> - R-Sm za FINU (REGOS)<br/> -
-						ID, IPP za Poreznu Upravu
-					</li>
-					<li>priprema ostalih obrazaca vezanih uz dohodak: IDD, IP, IDD-1</li>
-				</ul>
-			</div>
-			<div class="usluga">
-				<h3>Vođenje poslovnih knjiga</h3>
-				<ul>
-					<li>račun dobiti i gubitka (RDG), bilanca (BIL) i bilješke uz
-						financijske izvještaje</li>
-					<li>tromjesečni statistički izvještaj (TSI-POD)</li>
-					<li>godišnji financijski izvještaj (GFI-POD) za statistiku i javnu
-						objavu</li>
-					<li>prijava poreza na dobit (PD)</li>
-				</ul>
-			</div>
-			<div class="usluga">
-				<h3>Vođenje poslovnih knjiga</h3>
-				<ul>
-					<li>praćenje stanja obveza za javna davanja prema Poreznoj Upravi</li>
-					<li>saldakonti kupaca i dobavljača</li>
-					<li>izrada naloga za plaćanje (virmana)</li>
-					<li>vođenje nadzornih knjiga za poslovanje s inozemstvom</li>
-					<li>izvještavanje HNB-u o poslovima s inozemstvom</li>
-					<li>priprema ostalih izvještaja prema željama i potrebama klijenta</li>
-				</ul>
-			</div>
-		</div>
-		<div id="page3" class="page">
-			<h2>Klijenti</h2>
-			<p>
-				Nudimo kompletnu uslugu knjigovodstva i računovodstva za poduzeća,
-				obrte (standardne obrtnike i umjetnike) i udruge.<br/> Redovitim
-				praćenjem zakonskih i poreznih propisa te hrvatskih i međunarodnih
-				računovodstvenih standarda u <br/> mogućnosti smo pružiti stručan i
-				profesionalan pristup.
-			</p>
-			<div class="klijenti">
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-				<p>Klijent</p>
-			</div>
-		</div>
-		<div id="page4" class="page">
-			<h2>Kontakt</h2>
-			<div>
-				<p>
-					<span>tel:</span>+385 52 2389395
-				</p>
-				<p>
-					<span>mob:</span>+385 98 334466
-				</p>
-				<p>
-					<span>email:</span>icapara@gmail.com
-				</p>
-				<p>
-					<span>oib:</span>02410562314
-				</p>
-				<p>
-					<span>mb:</span>01026054
-				</p>
-				<p>
-					<span>adresa:</span> Posavski Ljudevita 2, Pula
-				</p>
-				<p>Bajta d.o.o.</p>
-				<div class="logo"></div>
-			</div>
-			<div>
-				<p class="lokacija">Pula, Croatia</p>
-				<div class="karta">
-					<iframe width="396" height="400" frameborder="0" scrolling="no"
-						marginheight="0" marginwidth="0"
-						src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=+Posavski+Ljudevita+2,+Pula&amp;aq=&amp;sll=44.881418,13.860626&amp;sspn=0.10497,0.209255&amp;ie=UTF8&amp;hq=&amp;hnear=Ulica+Ljudevita+Posavskog+2,+52100,+Pula,+Istarska+%C5%BEupanija,+Croatia&amp;t=m&amp;ll=44.860736,13.832817&amp;spn=0.097342,0.135612&amp;z=12&amp;iwloc=A&amp;output=embed"></iframe>
-				</div>
-			</div>
-		</div>
-		<div id="page5" class="page">
-			<h2>Linkovi</h2>
-			<div class="linkovi-left">
-				<p>www.adresa.com</p>
-				<p>www.adresa.com</p>
-				<p>www.adresa.com</p>
-				<p>www.adresa.com</p>
-				<p>www.adresa.com</p>
-			</div>
-			<div>
-				<p>www.adresa.com</p>
-				<p>www.adresa.com</p>
-				<p>www.adresa.com</p>
-				<p>www.adresa.com</p>
-				<p>www.adresa.com</p>
-			</div>
-		</div>
-		<div id="footer">
-			<p>
-				<span>Bajta d.o.o. &copy; 2013</span> <span class="powered">powered
-					by: web com</span>
-			</p>
-		</div>
-	</div>
+                    <li>praćenje stanja obveza za javna davanja prema Poreznoj Upravi</li>
+                    <li>saldakonti kupaca i dobavljača</li>
+                    <li>izrada naloga za plaćanje (virmana)</li>
+                    <li>vođenje nadzornih knjiga za poslovanje s inozemstvom</li>
+                    <li>izvještavanje HNB-u o poslovima s inozemstvom</li>
+                    <li>priprema ostalih izvještaja prema željama i potrebama klijenta</li>
+                </ul>
+        </div>
+    </div>
+    <div id="page3" class="page">
+    	<h2>Klijenti</h2>
+        <p>
+            Nudimo kompletnu uslugu knjigovodstva i računovodstva za poduzeća, obrte (standardne obrtnike i umjetnike) i udruge.<br/>
+            Redovitim praćenjem zakonskih i poreznih propisa te hrvatskih i međunarodnih računovodstvenih standarda u <br/>
+            mogućnosti smo pružiti stručan i profesionalan pristup.
+        </p>
+        <div class="klijenti">
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        	<p>Klijent</p>
+        </div>
+    </div>
+    <div id="page4" class="page">
+    	<h2>Kontakt</h2>
+        <div>
+        	<p><span>tel:</span>+385 52 2389395 </p>
+        	<p><span>mob:</span>+385 98 334466</p>
+        	<p><span>email:</span>icapara@gmail.com</p>
+        	<p><span>oib:</span>02410562314</p>
+        	<p><span>mb:</span>01026054 </p>
+            <p><span>adresa:</span> Posavski Ljudevita 2, Pula</p>
+            <p>Bajta d.o.o.</p>
+            <div class="logo"></div>
+        </div>
+        <div>
+			<p id="lokacija">Pula, Croatia</p>
+        	<div class="karta">
+        		<!-- TODO Napravit ćemo kartu kao div ne kao iframe -->
+            </div>
+        </div>
+    </div>
+    <div id="page5" class="page">
+    	<h2>Linkovi</h2>
+        <div class="linkovi-left">
+            <p>www.adresa.com</p>
+            <p>www.adresa.com</p>
+            <p>www.adresa.com</p>
+            <p>www.adresa.com</p>
+            <p>www.adresa.com</p>
+        </div>
+        <div>
+            <p>www.adresa.com</p>
+            <p>www.adresa.com</p>
+            <p>www.adresa.com</p>
+            <p>www.adresa.com</p>
+            <p>www.adresa.com</p>
+        </div>
+    </div>
+    <div class="footer">
+        <p>
+            <span>Bajta d.o.o. &copy; 2013</span>
+            <span class="powered">powered by: web com</span>
+        </p>
+    </div>
+</div>
 <script src="js/front.js"></script>
 </body>
 </html>
