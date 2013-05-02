802682e544e72c44345b25319b5f0bdaadbb50fb
web-com <luka.bracanovic@web-com.hr>
2013-05-02 22:17:00 +0200

    latest

--- js/front.js
+++ js/front.js
@@ -8,21 +8,26 @@ $("#header") >$(" ul li").click(function(e) {
 	 }, 800);
  });
  $(window).resize(function() {
-	 if($(window).width()<599){
-		 $("#header").append('<div id="menu-icon"></div>');
-	 }else{
-		$("#header").remove('#menu-icon');
+	 if($(window).width()<599 && !$("#menu-icon").is('*') ){
+		 createMenuIcon()
+	 }else if($("#menu-icon").is('*') && $(window).width()>599){
+		var vmenu=document.getElementById('menu-icon');
+		document.getElementById('header').removeChild(vmenu);
 	 }
 	 n=$("body").css("height").replace(/[^-\d\.]/g, '');
-	 if(n!=h){
+	 if(n!=h && $(window).width()>599){
 		$('body').animate({scrollTop: $("#page"+x).offset().top	}, 0);
 		 h=n;
 	 }
 });
 //------------------------------------------------------------------------------------ Helper functions
-$('#menu-icon').click(function(e){
-	alert(1);
-});
+function createMenuIcon(){
+	vmenu=document.createElement('div');
+	vmenu.id='menu-icon';
+	vmenu.onclick=function(){alert(1)}
+	$("#header").append(vmenu);
+	return vmenu;
+}
 
 //------------------------------------------------------------------------------------ Database
 
@@ -37,4 +42,7 @@ function bindingsInit(){
 //------------------------------------------------------------------------------------ document ready
 $(document).ready(function(){
 	bindingsInit();
+	if($(window).width()<599 && !$("#menu-icon").is('*')){
+		createMenuIcon();
+	}
 });
