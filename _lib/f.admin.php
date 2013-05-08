22701dc26feab2499dceaca6fc6d7f98e7f5ffe0
web-com <luka.bracanovic@web-com.hr>
2013-05-08 23:00:12 +0200

    Zadnje

--- _lib/f.admin.php
+++ _lib/f.admin.php
@@ -10,14 +10,26 @@ function navigation(){
 }
 
 function navigationStranice(){
+	global $get;
 	$stranice = db_dohvatiStranice();
 	foreach($stranice as $stranica){
-		$navCont.=wrap('<a href="'.navigationStranice_url($stranica->id).'">'.$stranica->naziv.'</a>','li');
+		$selected=$get->id==$stranica->id ?'selected':false;
+		$navCont.=wrap('<a href="'.navigationStranice_url($stranica->id).'">'.$stranica->naziv.'</a>','li',$selected);
 	}
 	return $navCont;
 }
 
 /* STRANICE */
+function navigationOstalo(){
+	return '
+		<li class="menu-right" >
+			<a href="" target="_new"><img src="../images/view.png" /> View site </a>
+			<a href="?logout=1"><img src="../images/log_out.png" /> Log out</a>
+        </li>';
+}
+function navigationStranice_url($stranicaId){
+	return '?t=stranice&id='.$stranicaId;
+}
 function db_dohvatiStranice(){
 	$sql="SELECT id,naziv FROM ".TBL."stranice";
 	$stranice=db::query_to_objects($sql);
@@ -39,4 +51,14 @@ function db_dohvatiPrijevod($id_stranice,$ln){
 function postojiPrijevod($id_stranice,$ln){
 	$stranica = db_dohvatiPrijevod($id_stranice,$ln);
 	return $stranica!==false;
+}
+
+function db_updatePrijevod($prijevod){
+	$sql="UPDATE ".TBL."sadrzaj SET naslov='".db::sqli($prijevod->naslov)."', sadrzaj='".htmlspecialchars($prijevod->sadrzaj)."', url='".db::sqli(prepareURI($prijevod->url,$delimiter='-'))."' WHERE id_stranice=".db::sqli($prijevod->id_stranice)." AND ln='".db::sqli($prijevod->ln)."'";
+	return db::query($sql);
+}
+
+function db_insertPrijevod($prijevod){
+	$sql="INSERT INTO ".TBL."sadrzaj(naslov,ln,sadrzaj,url,id_stranice) VALUES('".db::sqli($prijevod->naslov)."','".db::sqli($prijevod->ln)."','".htmlspecialchars($prijevod->sadrzaj)."','".db::sqli(prepareURI($prijevod->url,$delimiter='-'))."',".db::sqli($prijevod->id_stranice).")";
+	return db::query($sql);
 }
